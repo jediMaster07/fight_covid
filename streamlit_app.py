a605c2ec558043f728166e78613ccfd366fdc880
jediMaster07 <59928724+jediMaster07@users.noreply.github.com>
2021-05-03 22:41:28 +0530

    Removed index keyword from read_excel for pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,15 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 
 st.title("Fight CoVID - Resources!")
+st.write("For any updates/corrections/suggestions, please reach out to: ")
+st.markdown("***")
 
 choice = st.radio("Select the resource you need:", ("List of available beds", "Oxygen resources", "Remdevisir"))
 
 if choice == "List of available beds":
-    df = pd.read_excel(r"Resources/beds.xlsx", index=False)
-    
+    df = pd.read_excel(r"Resources/beds.xlsx")
     df = df.fillna("[Not Verified]")
-    
     st.table(df)
     
 elif choice == "Oxygen resources":
