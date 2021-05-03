798cf885376f7ae874bd42c17700a0c8dbc814b2
jediMaster07 <59928724+jediMaster07@users.noreply.github.com>
2021-05-03 23:20:01 +0530

    Removed email address from code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 st.title("Fight CoVID - Resources!")
-st.markdown("For any updates/corrections/suggestions, please reach out to: www.subhroneel_moitra@outlook.com")
+st.markdown("For any updates/corrections/suggestions, please reach out to:")
 st.markdown("***")
 
 choice = st.radio("Select the resource you need:", ("List of available beds", "Oxygen resources", "Remdevisir"))
