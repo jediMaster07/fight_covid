56380eb878fd33b09454c053dec56212a1040a0e
Subhroneel Moitra <59928724+jediMaster07@users.noreply.github.com>
2021-05-05 17:36:16 +0530

    Added option for home cooked meals and medicines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import streamlit as st
 
+st.set_page_config(page_icon="😷", initial_sidebar_state='collapsed')
+st.sidebar.write("Hell-o!")
+
 st.title("Fight CoVID - Resources!")
-st.markdown("For any updates/corrections/suggestions, please reach out to:")
+st.markdown("For any updates/corrections/suggestions, please reach out [here](mailto://fightcovid.resources@gmail.com   )")
+st.markdown("To volunteer with us connect [here](mailto://fightcovid.resources@gmail.com   )")
 st.markdown("***")
 
-choice = st.radio("Select the resource you need:", ("List of available beds", "Oxygen resources", "Remdevisir"))
+choice = st.radio("Select the resource you need:", ("List of available beds", "Oxygen resources", "Remdevisir", "Home-cooked meals", "Medicines"))
 
 if choice == "List of available beds":
     df = pd.read_excel(r"Resources/beds.xlsx")
@@ -18,6 +22,13 @@ elif choice == "Oxygen resources":
     st.table(df)
 
 elif choice == "Remdevisir":
+    st.write("Please give us some time we are gathering Remdevisir resources for this section")
+
+elif choice == "Home-cooked meals":
     st.write("Please give us some time we are gathering resources for this section")
+
+elif choice == "Medicines":
+    st.write("Please give us some time we are gathering medicine resources for this section")
+    
 
 else: pass
