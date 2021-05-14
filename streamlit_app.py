f0260b321720d287ffbc4c391ea78331d0a63378
Subhroneel Moitra <59928724+jediMaster07@users.noreply.github.com>
2021-05-14 21:52:08 +0530

    Added support for google sheets

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,30 +2,38 @@ import pandas as pd
 import streamlit as st
 
 st.set_page_config(page_icon="😷", initial_sidebar_state='collapsed')
-st.sidebar.write("Hell-o!")
+# st.sidebar.write("Our contributors:")
 
 st.title("Fight CoVID - Resources!")
 st.markdown("For any updates/corrections/suggestions, please reach out [here](mailto://fightcovid.resources@gmail.com   )")
 st.markdown("To volunteer with us connect [here](mailto://fightcovid.resources@gmail.com   )")
 st.markdown("***")
 
-choice = st.radio("Select the resource you need:", ("List of available beds", "Oxygen resources", "Remdevisir", "Home-cooked meals", "Medicines"))
+choice = st.radio("Select the resource you need:", ("Oxygen resources", "Home-cooked meals", "Medicines"))
 
 if choice == "List of available beds":
-    df = pd.read_excel(r"Resources/beds.xlsx")
-    df = df.fillna("[Not Verified]")
-    st.table(df)
+    pass
     
 elif choice == "Oxygen resources":
-    df = pd.read_excel(r"Resources/oxygen.xlsx")
-    df = df.fillna("[Not Verified]")
+    sheet_id = "1Oj64QgJYqvY65JYF_2pqnSXRQBlixGN8eyae-6lxjdU"
+    sheet_name = "Oxygen"
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+    
+    df = pd.read_csv(url)
+    df = df.fillna("")
     st.table(df)
 
 elif choice == "Remdevisir":
     st.write("Please give us some time we are gathering Remdevisir resources for this section")
 
-elif choice == "Home-cooked meals":
-    st.write("Please give us some time we are gathering resources for this section")
+elif choice == "Home-cooked meals":    
+    sheet_id = "1sgHGi1ns0dmVtmRdWVYb9Ax9LChurlkJJlLf1YeH_bE"
+    sheet_name = "Meals"
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+    
+    df = pd.read_csv(url)
+    df = df.fillna("")
+    st.table(df)
 
 elif choice == "Medicines":
     st.write("Please give us some time we are gathering medicine resources for this section")
